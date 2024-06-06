2b5a7d6c0278fa354bce5a0cef5287a53e1b7641
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-06 09:48:06 +0600

    "Added div elements with class 'lineline' and 'lineline2' wrapping h1 elements in What.jsx, and added corresponding CSS styles to what.css"

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -6,12 +6,16 @@ const What = () => {
     <div className="flex justify-center py-10">
       <div className='bg-blue-900 p-2 md:p-7 py-10 w-[80%]'>
         <div className="title p-4 flex flex-col md:flex-row justify-center ">
-          <h1 className=' text-subtext text-xl md:text-4xl w-[100%] md:w-[50%]'>What is GPT?</h1>
+          <div className="lineline w-[100%] md:w-[50%]">
+            <p></p>
+            <h1 className='text-subtext text-xl md:text-4xl'>What is GPT?</h1>
+          </div>
           <p className='text-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam porro similique illum voluptate quia natus, autem recusandae incidunt voluptas quod accusantium neque vel ducimus cumque, expedita tempore quos, soluta nulla.</p>
-          
+
         </div>
 
         <div className="bigbrief py-10 p-4 flex flex-col md:flex-row justify-center gap-5">
+
           <h1 className=' text-subtext text-4xl font-bold'>The Possibilities are beyond your imagination.</h1>
           <section className='flex flex-col'>
 
@@ -22,15 +26,25 @@ const What = () => {
 
         <div className="what3feat py-10 p-4 flex flex-col md:flex-row gap-5">
           <div className="">
-            <h1 className='text-subtext text-2xl font-bold'>Chatbots</h1>
+            <div className="lineline2">
+              <p></p>
+              <h1 className='text-subtext text-2xl font-bold'>Chatbots</h1>
+            </div>
+
             <p className='text-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam porro similique illum voluptate quia natus, autem recusandae incidunt voluptas quod accusantium neque vel ducimus cumque, expedita tempore quos, soluta nulla.</p>
           </div>
           <div className="">
-            <h1 className=' text-subtext text-2xl font-bold'>Image Generation</h1>
+          <div className="lineline2">
+              <p></p>
+              <h1 className='text-subtext text-2xl font-bold'>Image Generation</h1>
+            </div>
             <p className='text-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam porro similique illum voluptate quia natus, autem recusandae incidunt voluptas quod accusantium neque vel ducimus cumque, expedita tempore quos, soluta nulla.</p>
           </div>
           <div className="">
-            <h1 className=' text-subtext text-2xl font-bold'>Programming</h1>
+          <div className="lineline2">
+              <p></p>
+              <h1 className='text-subtext text-2xl font-bold'>Programming</h1>
+            </div>
             <p className='text-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam porro similique illum voluptate quia natus, autem recusandae incidunt voluptas quod accusantium neque vel ducimus cumque, expedita tempore quos, soluta nulla.</p>
           </div>
 
