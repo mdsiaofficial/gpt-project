931f5c8511cfa3cc7fd380d1966d3b886dc67569
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-07 16:50:37 +0600

    Revert ""Updated Article component to accept imgURL, date, and title props; passed props from Blog component; and changed blog05 asset from svg to png.""
    This reverts commit cc3eda4de31d6ba1a535ef538b300396019f72a7.

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import "./article.css";
 
-const Article = (imgURL, date, title) => {
+const Article = () => {
   return (
     <div>
       <div className="blog_article">
         <div className="article_img">
-          <img src={imgURL} alt="" />
+          
         </div>
-
-
-        <div className="article_content">
-          <p>{date}</p>
-          <h3>{title}</h3>
-        </div>
-
-        <p>Read Full Article</p>
       </div>
     </div>
   )
